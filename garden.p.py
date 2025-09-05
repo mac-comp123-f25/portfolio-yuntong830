926baae451fcc1fdebe72ccc6ea79a9a2fb276b6
yuntong830 <yli14@macalester.edu>
2025-09-05 00:47:40 -0500

    Create garden.p.py

---
+++ garden.p.py
@@ -0,0 +1,8 @@
+bed_len = 10
+bed_width = 2
+bed_len_inch = bed_len*12
+bed_width_inch = bed_width*12
+bed_rows = bed_len_inch/6
+bed_columns = bed_width_inch/6
+flowers = bed_rows*bed_columns
+print(flowers)
