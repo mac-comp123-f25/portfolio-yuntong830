71a5c6ff5f20117ef31160da649c31832c39577b
yuntong830 <yli14@macalester.edu>
2025-10-14 12:10:26 -0500

    Update tax.py

--- tax.py
+++ tax.py
@@ -1,5 +1,8 @@
 def add_tax(price, tax_rate):
+    print('add_tax inputs:', 'price =', price, 'tax_rate =', tax_rate)
     tax_amt = price * tax_rate
+    total = price + tax_amt
+    print('total after tax:', total)
     return price + tax_amt
 print(add_tax(25.99,0.0725))
 '''
