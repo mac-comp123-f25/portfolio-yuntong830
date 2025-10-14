9fc87c99aa8774c484cc1ffe5fe21cc7dd83fc29
yuntong830 <yli14@macalester.edu>
2025-10-14 12:07:40 -0500

    Update difference.py

--- difference.py
+++ difference.py
@@ -1,12 +1,14 @@
 def smallest_diff(x, y, z):
+    print('smallest_diff inputs:', x, y, z)
     diff1 = abs(x - y)
     diff2 = abs(y - z)
     diff3 = abs(x - z)
     min_diff = min(diff1, diff2, diff3)
+    print(diff1, diff2, diff3, 'return value:', min_diff)
     return min_diff
 print(smallest_diff(32,43,90))
 '''
-Local environmen
+Local environment
    x    32
    y    43
    z    90
