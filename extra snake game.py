bb1bf0b45737ce12e62a130b117bfe5726885e80
yuntong830 <yli14@macalester.edu>
2025-12-10 01:37:28 -0600

    extra

---
+++ extra snake game.py
@@ -0,0 +1,105 @@
+import turtle
+import time
+import random
+
+delay = 0.1
+win = turtle.Screen()
+win.title("Simple Snake Game")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+win.listen()
+win.onkeypress(go_up, "w")
+win.onkeypress(go_down, "s")
+win.onkeypress(go_left, "a")
+win.onkeypress(go_right, "d")
+while True:
+    win.update()
+
+
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        for seg in segments:
+            seg.goto(1000, 1000)
+        segments.clear()
+
+
+    if head.distance(food) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        food.goto(x, y)
+
+        new_seg = turtle.Turtle()
+        new_seg.speed(0)
+        new_seg.shape("square")
+        new_seg.color("grey")
+        new_seg.penup()
+        segments.append(new_seg)
+
+
+    for i in range(len(segments)-1, 0, -1):
+        segments[i].goto(segments[i-1].xcor(), segments[i-1].ycor())
+
+    if segments:
+        segments[0].goto(head.xcor(), head.ycor())
+
+    move()
+
+    for seg in segments:
+        if seg.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            for seg in segments:
+                seg.goto(1000, 1000)
+            segments.clear()
+
+    time.sleep(delay)
